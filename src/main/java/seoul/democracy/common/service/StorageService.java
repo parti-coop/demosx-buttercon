c76eba77ddaff44497bf89dbb261d94f32f80691
Swain Hwang <swain@swainuicbookpro.lan>
2020-02-20 04:54:37 -0800

    s3 연결

--- src/main/java/seoul/democracy/common/service/StorageService.java
+++ src/main/java/seoul/democracy/common/service/StorageService.java
@@ -21,16 +21,39 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.mysema.util.FileUtils;
+
 @Slf4j
 @Service
 public class StorageService {
 
     private final String systemUploadPath;
     private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM");
+    private final String bucketName = "butterknifecrew";
+    private final String AWS_ACCESS_KEY;
+    private final String AWS_SECRET_KEY;
+    private final AmazonS3 s3client;
 
     @Autowired
     public StorageService(EgovPropertyService propertyService) {
         this.systemUploadPath = propertyService.getString("uploadFilePath");
+        this.AWS_ACCESS_KEY = propertyService.getString("AWS_ACCESS_KEY");
+        this.AWS_SECRET_KEY = propertyService.getString("AWS_SECRET_KEY");
+        if (StringUtils.hasLength(this.AWS_ACCESS_KEY + this.AWS_SECRET_KEY)) {
+            AWSCredentials credentials = new BasicAWSCredentials(this.AWS_ACCESS_KEY, this.AWS_SECRET_KEY);
+            s3client = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials))
+                    .withRegion(Regions.AP_NORTHEAST_2).build();
+        } else {
+            s3client = AmazonS3ClientBuilder.standard().withRegion(Regions.AP_NORTHEAST_2).build();
+        }
         log.info("uploadFilePath : [{}]", systemUploadPath);
     }
 
@@ -41,36 +64,50 @@ public class StorageService {
 
         /* save file */
         try {
-            String filename = UUID.randomUUID().toString().replace("-", "") + "." +
-                                  StringUtils.getFilenameExtension(multipartFile.getOriginalFilename());
+            String originalFileName = multipartFile.getOriginalFilename();
+            String filename = UUID.randomUUID().toString().replace("-", "") + "."
+                    + StringUtils.getFilenameExtension(originalFileName);
 
             String uploadPath = LocalDate.now().format(dateTimeFormatter);
 
             File file = new File(this.systemUploadPath + uploadPath, filename);
-            if (!file.getParentFile().exists()) file.getParentFile().mkdirs();
+            if (!file.getParentFile().exists())
+                file.getParentFile().mkdirs();
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(multipartFile.getSize());
+            if (type == UploadFileType.ORIGINAL) {
 
-            if (type == UploadFileType.ORIGINAL)
                 FileCopyUtils.copy(multipartFile.getBytes(), file);
-            else {
+                // request = new PutObjectRequest(bucketName, uploadPath + "/" + filename,
+                // multipartFile.getInputStream(),
+                // metadata);
+
+            } else {
                 BufferedImage sourceImg = Thumbnails.of(multipartFile.getInputStream()).scale(1).asBufferedImage();
                 if (sourceImg == null)
                     throw new BadRequestException("file", "error.file", "이미지 파일이 아닙니다.");
 
                 if (type.getHeight() == 0) {
                     if (sourceImg.getWidth() > type.getWidth()) {
-                        Thumbnails.of(sourceImg).width(type.getWidth()).toFile(file);
+                        Thumbnails.of(sourceImg).width(type.getWidth()).asBufferedImage();
                     } else {
                         Thumbnails.of(sourceImg).scale(1).toFile(file);
                     }
                 } else {
-                    Thumbnails.of(sourceImg).size(type.getWidth(), type.getHeight())
-                        .crop(Positions.CENTER)
-                        .keepAspectRatio(true)
-                        .toFile(file);
+                    Thumbnails.of(sourceImg).size(type.getWidth(), type.getHeight()).crop(Positions.CENTER)
+                            .keepAspectRatio(true).toFile(file);
                 }
             }
-
-            return UploadFileInfo.of(multipartFile.getOriginalFilename(), "/files/" + uploadPath + "/" + filename);
+            String url = "/files/" + uploadPath + "/" + filename;
+            try {
+                PutObjectRequest request = new PutObjectRequest(bucketName, url.substring(1), file);
+                s3client.putObject(request);
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            } finally {
+                FileUtils.delete(file);
+            }
+            return UploadFileInfo.of(originalFileName, url);
         } catch (IOException e) {
             log.info("{}", e);
             throw new BadRequestException("file", "error.file", e.getMessage());
