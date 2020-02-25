3a0d083ffb0b8a6f6a0b5339df2796c756e32f64
Swain Hwang <swain@swainuicbookpro.lan>
2020-02-25 01:48:04 -0800

    remove credential

--- src/main/java/seoul/democracy/common/service/StorageService.java
+++ src/main/java/seoul/democracy/common/service/StorageService.java
@@ -1,10 +1,8 @@
 package seoul.democracy.common.service;
 
-import egovframework.rte.fdl.property.EgovPropertyService;
 import lombok.extern.slf4j.Slf4j;
 import net.coobird.thumbnailator.Thumbnails;
 import net.coobird.thumbnailator.geometry.Positions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,9 +24,6 @@ import javax.imageio.ImageIO;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
@@ -38,27 +33,9 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 @Service
 public class StorageService {
 
-    // private final String systemUploadPath;
     private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM");
     private final String bucketName = "butterknifecrew";
-    private final String AWS_ACCESS_KEY;
-    private final String AWS_SECRET_KEY;
-    private final AmazonS3 s3client;
-
-    @Autowired
-    public StorageService(EgovPropertyService propertyService) {
-        // this.systemUploadPath = propertyService.getString("uploadFilePath");
-        this.AWS_ACCESS_KEY = propertyService.getString("AWS_ACCESS_KEY");
-        this.AWS_SECRET_KEY = propertyService.getString("AWS_SECRET_KEY");
-        if (StringUtils.hasLength(this.AWS_ACCESS_KEY + this.AWS_SECRET_KEY)) {
-            AWSCredentials credentials = new BasicAWSCredentials(this.AWS_ACCESS_KEY, this.AWS_SECRET_KEY);
-            s3client = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials))
-                    .withRegion(Regions.AP_NORTHEAST_2).build();
-        } else {
-            s3client = AmazonS3ClientBuilder.standard().withRegion(Regions.AP_NORTHEAST_2).build();
-        }
-        // log.info("uploadFilePath : [{}]", systemUploadPath);
-    }
+    private final AmazonS3 s3client = AmazonS3ClientBuilder.standard().withRegion(Regions.AP_NORTHEAST_2).build();
 
     public UploadFileInfo store(UploadFileType type, MultipartFile multipartFile) {
         if (multipartFile.isEmpty()) {
@@ -109,7 +86,6 @@ public class StorageService {
                 iStream = new ByteArrayInputStream(os.toByteArray());
             }
             String url = "/files/" + uploadPath + "/" + filename;
-           
             try {
                 PutObjectRequest request = new PutObjectRequest(bucketName, url.substring(1), iStream, metadata);
                 request.setCannedAcl(CannedAccessControlList.PublicRead);
